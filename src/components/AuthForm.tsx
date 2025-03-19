164960d429f7dbb83dae6aaef28b3462594fe2e8
prashxant <notxdead0@gmail.com>
2025-03-19 19:36:09 +0530

    wow to much coding happening

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,7 +1,12 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import React from 'react'
-import { CardContent } from './ui/card';
+import React, { useTransition } from 'react'
+import { CardContent, CardFooter } from './ui/card';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { Loader2 } from 'lucide-react';
+import Link from 'next/link';
 
 
 type props = {
@@ -14,6 +19,7 @@ function AuthForm({type }:props) {
 const isloginForm = type === "login";
 
 const router = useRouter();
+const[isPending,startTransition] = useTransition()
 
 const handleSubmit = (formData:FormData) =>{
     console.log("Form Submiited");
@@ -21,11 +27,46 @@ const handleSubmit = (formData:FormData) =>{
 
   return ( 
     <form action={handleSubmit}>
-        <CardContent>
-            <div>
-                
+        <CardContent className="grid w-full item-center gap-4">
+        <div className="flex flex-col space-y-1.5">
+              <Label htmlFor='"email'>Email</Label>  
+              <Input
+              id="email"
+              name="email"
+              placeholder="enter your enmail"
+              type="email"
+              required
+              disabled={isPending}              
+              />
+              
+            </div><div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>  
+              <Input
+              id="password"
+              name="password"
+              placeholder="enter your password"
+              type="password"
+              required
+              disabled={isPending}              
+              />
+              
             </div>
         </CardContent>
+        <CardFooter>
+            <Button>
+                {isPending ? 
+                    <Loader2 className='animate-spin'/>
+                : isloginForm ?(
+                 "Login")
+                  :( "Sign Up ")}
+            </Button>
+            <p className='text-xs'>
+                {isloginForm?"Don't have an account? ":"Already have an account? "}{" "}
+                <Link href={isloginForm?"/sign-up":"/login"} className={`text-blue-500 underline ${isPending? "pointer-events-none opacity-50":""}`} >
+                {isloginForm?"Sign Up":"Login"} 
+                </Link>
+            </p>
+        </CardFooter> 
     </form>
   )
 }
