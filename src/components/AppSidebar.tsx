52609679d0776147cdc2a8dec1034074e03472f5
prashxant <notxdead0@gmail.com>
2025-03-20 19:54:41 +0530

    added side bar

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,24 @@
+import { getUser } from "@/auth/server"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+  } from "@/components/ui/sidebar"
+  
+  async function AppSidebar() {
+    const user  = getUser()
+    return (
+      <Sidebar>
+        <SidebarHeader />
+        <SidebarContent>
+          <SidebarGroup />
+          <SidebarGroup />
+        </SidebarContent>
+        <SidebarFooter />
+      </Sidebar>
+    )
+  }
+  export default AppSidebar 
+  
